1cf28679eeb975b66e9ab7dd86743f84a69074fe
Yashaswini2004 <yashaswinic31@gmail.com>
2023-10-21 16:23:14 +0530

    New lottery program

--- lottery1.py
+++ lottery1.py
@@ -1,12 +1,12 @@
 import random
 account=0
-for i in range(7):
+for i in range(30):
     bet=random.randint(1,10)
     lucky_draw=random.randint(1,10)
-    print("Bet:",bet)
-    print("Lucky draw:",lucky_draw)
+    #print("Bet:",bet)
+    #print("Lucky draw:",lucky_draw)
     if bet==lucky_draw:
         account=account+900-100
     else:
         account=account-100
-    print("YOUR ACCOUNT BALANCE:",account)
+print("YOUR ACCOUNT BALANCE:",account)
